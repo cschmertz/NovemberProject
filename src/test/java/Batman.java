af6992abc7d730aeafb52aa1708774b159db8162
cschmertz <cschmertz@gmail.com>
2020-11-01 10:54:47 -0500

    Batman message2

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -1,5 +1,6 @@
 public class Batman {
     public static void main(String[] args) {
         System.out.println("I will catch you");
+        System.out.println("This city is needs a new breed of criminal");
     }
 }
